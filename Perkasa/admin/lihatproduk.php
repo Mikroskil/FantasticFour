4435d05213a6929cc3e78dbf6865f640dd57c4e0
mariobonardo <mariobonardo04@gmail.com>
2014-01-21 22:50:32 +0700

    rev

--- Perkasa/admin/lihatproduk.php
+++ Perkasa/admin/lihatproduk.php
@@ -29,13 +29,13 @@ else {
 
 							<table border="0" align='center' cellspacing='1'>
 
-								<tr><th>Id</th><th>Nama</th><th>Harga</th><th>Stok</th><th>Added</th><th>Gbr</th><th>Option</th></tr>
+								<tr><th>Id</th><th>Nama</th><th>Harga</th><th>Stok</th><th>Keterangan</th><th>Added</th><th>Gbr</th><th>Option</th></tr>
 
 									<?php
 										 include '../include/connect.php';
 										 $query=mysql_query("select * from  produk",$con);
 										 while ($x=mysql_fetch_array($query)){
-									 	echo "<tr><td>".$x[0]."</td><td>".$x[1]."</td><td>".$x[2]."</td><td>".$x[3]."</td><td>".$x[6]."</td>
+									 	echo "<tr><td>".$x[0]."</td><td>".$x[1]."</td><td>".$x[2]."</td><td>".$x[3]."</td><td>".$x[4]."</td><td>".$x[6]."</td>
 					 					<td>";
 					 					if($x[5]!="noimg.jpg") echo  ("Y");else print("-");
 					 					echo "</td><td><span class='lihatproduk'><a href='editproduk.php?id=".$x[0]."'>Edit</a> <br> <a href='hapusprod.php?id=".$x[0]."'>Hapus</a></span></td></tr>";
